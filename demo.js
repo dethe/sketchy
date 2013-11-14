5402d80880e77de7f0c07ed0b42ed3587f0c2487
dethe <dethe@livingcode.org>
2013-11-14 15:25:37 -0800

    updated demo

--- demo.js
+++ demo.js
@@ -2,5 +2,5 @@ function toArray(list){
     return Array.prototype.slice.call(list);
 }
 toArray(document.getElementsByTagName('code')).forEach(function(elem){
-    eval(elem.innerText);
+    eval(elem.textContent);
 });
